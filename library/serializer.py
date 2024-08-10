fb870168edb4d782ee8721c98d49a914ddaac445
Kris <ssspers92@gmail.com>
2024-08-10 14:09:47 +0300

    Created LogServiceSerializer

--- library/serializer.py
+++ library/serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from library.models import Author, Book
+from users.models import User
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -11,6 +12,15 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class BookSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all(), many=True)
+
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+
+class LogServiceSerializer(serializers.ModelSerializer):
+    book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=True)
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
 
     class Meta:
         model = Book
