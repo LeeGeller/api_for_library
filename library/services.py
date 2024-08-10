5f162fb5b50a152622593460bb367219ac4b8a7a
Kris <ssspers92@gmail.com>
2024-08-10 14:34:38 +0300

    Changed return

--- library/services.py
+++ library/services.py
@@ -2,5 +2,4 @@ def check_count_of_book(books: list) -> bool | str:
     for book in books:
         if book['count'] > 0:
             return True
-        else:
-            return f'Книга {book} закончилась'
+    return False
